dd5ae571dc4b2400389858790e442a21dfbce0a8
pedrogrande <pete@peterargent.com>
2024-12-14 18:53:52 +1100

    initial commit

---
+++ app/controllers/public_controller.rb
@@ -0,0 +1,11 @@
+class PublicController < ApplicationController
+  def home
+    @msg = "Hello Vue on Rails!"
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
---
+++ app/frontend/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  msg: String
+})
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  color: red;
+}
+</style>
